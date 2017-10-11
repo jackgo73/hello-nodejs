b1ab03a05128bf47ed6daf441a10feea560e4e9d
Jack Go <mutex73@gmail.com>
2017-10-11 15:33:17 +0800

    Signed-off-by: Jack Go <mutex73@gmail.com>

--- hello.go
+++ hello.go
@@ -9,15 +9,16 @@ import (
 )
 
 type Vertex struct {
-	X int
-	Y int
+	X, Y int
 }
 
+var (
+	v1 = Vertex{1, 2}  // 类型为 Vertex
+	v2 = Vertex{X: 1}  // Y:0 被省略
+	v3 = Vertex{}      // X:0 和 Y:0
+	p  = &Vertex{1, 2} // 类型为 *Vertex
+)
+
 func main() {
-	v := Vertex{1,2}
-	v.X = 4
-
-	p := &v
-
-	fmt.Println(p.X)
+	fmt.Println(v1, p, v2, v3)
 }
