f28c11e8cac7accca52bee1bb081063479285416
Jack Go <mutex73@gmail.com>
2017-10-11 15:41:56 +0800

    Signed-off-by: Jack Go <mutex73@gmail.com>

--- hello.go
+++ hello.go
@@ -12,7 +12,7 @@ func main() {
 	p := []int{2, 3, 5, 7, 11, 13}
 	fmt.Println(p)
 
-	for i := 0; i < len(p); i++ {
-		fmt.Println(i, p[i])
-	}
+	fmt.Println(p[1:4])
+	fmt.Println(p[:3])
+	fmt.Println(p[4:])
 }
