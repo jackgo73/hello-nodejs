166f00f4bdd8f844dc2c322d1074c228f930a186
Jack Go <mutex73@gmail.com>
2017-10-11 16:23:21 +0800

    map

--- hello.go
+++ hello.go
@@ -4,29 +4,19 @@
 */
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-
-func main() {
-	var a []int
-	printSlice("a", a)
-
-	// append works on nil slices.
-	a = append(a, 0)
-	printSlice("a", a)
-
-	// the slice grows as needed.
-	a = append(a, 1)
-	printSlice("a", a)
-
-	// we can add more than one element at a time.
-	a = append(a, 2, 3, 4)
-	printSlice("a", a)
+type Vertex struct {
+	Lat, Long float64
 }
 
-func printSlice(s string, x []int) {
-	fmt.Printf("%s len=%d cap=%d %v\n",
-		s, len(x), cap(x), x)
+var m map[string]Vertex
+
+func main() {
+	m = make(map[string]Vertex)
+	m["Bell"] = Vertex{
+		40,-74,
+	}
+	fmt.Println(m["Bell"])
+
 }
