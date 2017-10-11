7e52bc68f4b13276d28eb2b3c2481e2280748541
Jack Go <mutex73@gmail.com>
2017-10-11 16:12:56 +0800

    append slice

--- hello.go
+++ hello.go
@@ -10,9 +10,23 @@ import (
 
 
 func main() {
-	var z []int
-	fmt.Println(z, len(z), cap(z))
-	if z == nil {
-		fmt.Println("nil!")
-	}
+	var a []int
+	printSlice("a", a)
+
+	// append works on nil slices.
+	a = append(a, 0)
+	printSlice("a", a)
+
+	// the slice grows as needed.
+	a = append(a, 1)
+	printSlice("a", a)
+
+	// we can add more than one element at a time.
+	a = append(a, 2, 3, 4)
+	printSlice("a", a)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
 }
