04a5d2fe4e12416fa59ed061d58b358d8a5399e1
Jack Go <mutex73@gmail.com>
2017-10-11 15:37:02 +0800

    Signed-off-by: Jack Go <mutex73@gmail.com>

--- hello.go
+++ hello.go
@@ -12,13 +12,10 @@ type Vertex struct {
 	X, Y int
 }
 
-var (
-	v1 = Vertex{1, 2}  // 类型为 Vertex
-	v2 = Vertex{X: 1}  // Y:0 被省略
-	v3 = Vertex{}      // X:0 和 Y:0
-	p  = &Vertex{1, 2} // 类型为 *Vertex
-)
-
 func main() {
-	fmt.Println(v1, p, v2, v3)
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0],a[1])
+	fmt.Println(a)
 }
