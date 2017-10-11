c6dfc3d7079a449ba39068b0ed6358ce30b23c39
Jack Go <mutex73@gmail.com>
2017-10-11 15:39:23 +0800

    Signed-off-by: Jack Go <mutex73@gmail.com>

--- hello.go
+++ hello.go
@@ -8,14 +8,11 @@ import (
 	"fmt"
 )
 
-type Vertex struct {
-	X, Y int
-}
-
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0],a[1])
-	fmt.Println(a)
+	p := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(p)
+
+	for i := 0; i < len(p); i++ {
+		fmt.Println(i, p[i])
+	}
 }
