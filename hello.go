5ed54ecc19e480388d158fedd10e1d80c744846b
Jack Go <mutex73@gmail.com>
2017-10-12 10:53:10 +0800

    web

--- hello.go
+++ hello.go
@@ -6,41 +6,22 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"log"
+	"net/http"
 )
 
-type Abser interface {
-	Abs() float64
+type Hello struct{}
+
+func (h Hello) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, "Hello!")
 }
 
 func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	a = f  // a MyFloat 实现了 Abser
-	a = &v // a *Vertex 实现了 Abser
-
-	// 下面一行，v 是一个 Vertex（而不是 *Vertex）
-	// 所以没有实现 Abser。
-	a = v
-
-	fmt.Println(a.Abs())
-}
-
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
+	var h Hello
+	err := http.ListenAndServe("localhost:4010", h)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return float64(f)
-}
-
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
