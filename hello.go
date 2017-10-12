d7d9ed71a36e1252a99d01f452441726c301ccea
Jack Go <mutex73@gmail.com>
2017-10-12 09:17:03 +0800

    method

--- hello.go
+++ hello.go
@@ -9,8 +9,14 @@ import (
 	"fmt"
 )
 
+
 type Vertex struct {
 	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
 }
 
 func (v *Vertex) Abs() float64 {
@@ -18,6 +24,7 @@ func (v *Vertex) Abs() float64 {
 }
 
 func main() {
-	v := Vertex{3, 4}
-	fmt.Println(v.Abs())
+	v := &Vertex{3, 4}
+	v.Scale(5)
+	fmt.Println(v, v.Abs())
 }
